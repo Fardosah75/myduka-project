574de142345b5becfeee57a490d855177c6cc458
fardosa <fardousah011@gmail.com>
2025-10-28 16:55:08 +0300

    added a navbar and a modal,created aroute to receive data and add data from the form on the front end  and commit to the database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #import the class Flask from the framework flask
-from flask import Flask,render_template
-from database import fetch_data
+from flask import Flask,render_template,request,redirect,url_for
+from database import fetch_data,insert_products
 #instance of the class(object)
 app=Flask(__name__)
 
@@ -15,6 +15,29 @@ def products():
     
     return render_template ('products.html',product=prods)
 
+#create a route to add products/receive products from the ui to the serverside
+@app.route('/add_products',methods=['GET','POST']) 
+#create the function to receive data from the modal on the form 
+def add_products():
+    
+    #check the method used,import request from flask
+    if request.method=='POST':
+        #get the inputs from the form using name attributes and store them in a variable
+        pname=request.form['name']
+        bp=request.form['buyingprice']
+        sp=request.form['sellingprice']
+
+        #store the new products in a variable
+        new_product=(pname,bp,sp)
+        # import from database.py &call the insert function to insert into the database
+        insert_products(new_product)
+        #redirect to the product page to see the inserted value
+    return redirect(url_for('products'))
+
+
+         
+
+
 @app.route('/sales')
 def sales():
     sale=fetch_data('sales')
@@ -26,4 +49,4 @@ def stock():
     #print(stocks)
     return render_template ('stock.html',stocks=stocks)
 
-app.run()
+app.run(debug=True)
