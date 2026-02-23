95e9cb772254d2b2eef53aa93c28829d7127609f
fardosa <fardousah011@gmail.com>
2026-02-23 22:43:54 +0300

    updated dashboard and  addeded session

--- database.py
+++ database.py
@@ -104,3 +104,127 @@ def product_sales():
     return sales
 #mysale=product_sales()
 #print(f'my product sales is {mysale}')
+
+
+def total_sales():
+    query=' select sum(quantity*selling_price) as total_sales from products as p inner join sales as s on p.id=s.pid;'
+    curr.execute(query)
+    sale=curr.fetchone()
+    return sale[0]
+
+    
+#mytotal=total_sales()
+#mytotal=(mytotal)
+#print(f'my total sale is ({mytotal}')
+
+#total profit
+def total_profit():
+    query=' select sum(total_profit) as total_profit from(select p.id, sum((p.selling_price - p.buying_price)*s.quantity) as total_profit from products as p join sales as s on p.id=s.pid group by p.id);'
+    curr.execute(query)
+    profit=curr.fetchone()
+    return profit[0]
+#profit per product
+def get_profit():
+    query='select p.name,p.id, sum((p.selling_price - p.buying_price)* s.quantity) as profits from products as p join sales as s on p.id=s.pid group by p.name,p.id;'
+    curr.execute(query)
+    profits=curr.fetchall()
+    return profits
+profit=get_profit()
+#print(f'my profit is{profit}')
+
+#sales per product
+def get_sale():
+    query=' select p.name, sum(quantity*selling_price) as total_sales from products as p inner join sales as s on p.id=s.pid group by p.name;'
+    curr.execute(query)
+    sales=curr.fetchall()
+    return sales
+#sale=get_sale()
+#print(f'my sales per product is {sale}')
+
+
+#sales per day
+def sales_day():
+    query='select DATE(s.created_at) as day, sum(p.selling_price*s.quantity) from products as p join sales as s on p.id=s.pid group by DATE(s.created_at);'
+    curr.execute(query)
+    data=curr.fetchall()
+    return data
+#dsale=sales_day()
+#print(dsale)
+
+#profit per day
+def profit_day():
+    query='select DATE(s.created_at) as day, sum((p.selling_price - p.buying_price)*s.quantity) from products as p join sales as s on p.id=s.pid group by DATE(s.created_at);'
+    curr.execute(query)
+    data1=curr.fetchall()
+    return data1
+#total profit per day
+def tprofit_day():
+    query=' select  sum((p.selling_price - p.buying_price)*s.quantity) from products as p join sales as s on p.id=s.pid where DATE(s.created_at)=CURRENT_DATE'
+    curr.execute(query)
+    data2=curr.fetchone()
+    return data2 [0]
+
+#TOTAL SALE PER DAY
+def tsale_day():
+    query='select sum(p.selling_price*s.quantity) from products as p join sales as s on p.id=s.pid where DATE(s.created_at)=CURRENT_DATE;'
+    curr.execute(query)
+    data3=curr.fetchone()
+    return data3 [0]
+
+def month():
+    query='select DISTINCT  Extract(month from s.created_at) as month_name from sales as s;'
+    curr.execute(query)
+    data4=curr.fetchall()
+    return data4
+
+def insert_users(values):
+    query='insert into users(full_name,email,password) values(%s,%s,%s)'
+    curr.execute(query,values)
+    conn.commit()
+
+def check_email(email):
+    query='select * from users where email=%s'
+    #the comma and brackests makes the email a tuple
+    curr.execute(query,(email,))
+    data=curr.fetchone()
+    return data
+
+
+def update_product(a,b,c,d):
+    query="update products set name=%s, buying_price=%s, selling_price=%s where id=%s"
+    curr.execute(query,(a,b,c,d))
+    conn.commit()
+
+def update_sales(a,b):
+    query="update sales set quantity=%s where id=%s"
+    curr.execute(query,(a,b))
+    conn.commit()   
+
+
+def recent_sales():
+    query='select s.id,p.name,s.created_at from sales as s join products as p on p.id=s.pid group by p.name,s.id,p.id order by created_at desc limit 10;'
+    curr.execute(query)
+    data4=curr.fetchall()
+    return data4    
+
+def top_selling():
+    query=' select p.name,p.id,count (s.id) from sales as s join products as p on p.id=s.pid group by p.name, p.id order by count(s.id) desc limit 10 ;'
+    curr.execute(query)
+    data5=curr.fetchall()
+    return data5
+
+
+def update_stock(a,b):
+    query="update stock set stock_quantity=%s where id=%s"
+    curr.execute(query,(a,b))
+    conn.commit()  
+
+
+    
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 #import the class Flask from the framework flask
-from flask import Flask,render_template,request,redirect,url_for
-from database import fetch_data,insert_products,insert_sales,insert_stock
+from flask import Flask,render_template,request,redirect,url_for,session,flash
+from database import fetch_data,insert_products,insert_sales,insert_stock,total_sales,total_profit,get_profit,get_sale,sales_day, profit_day,tprofit_day,tsale_day,month,insert_users,check_email,update_product,update_sales,recent_sales,top_selling,update_stock
+
+from flask_bcrypt import Bcrypt
+
 #instance of the class(object)
 app=Flask(__name__)
+#instance of the class-Bycrypt(object)
+bycrypt=Bcrypt(app)
+app.secret_key='remem2'
 
 #creating a route
 @app.route('/')
 def home():
+
+
     return render_template ('index.html')
 
 @app.route('/products')
 def products():
-    prods=fetch_data('products')
+    if 'email' in session:
+
+        prods=fetch_data('products')
+    else:
+        flash('log in to access this page',"warning")    
+        return redirect (url_for('log_in'))
     
     return render_template ('products.html',product=prods)
 
@@ -31,8 +44,32 @@ def add_products():
         new_product=(pname,bp,sp)
         # import from database.py &call the insert function to insert into the database
         insert_products(new_product)
+        flash("product added successfully","info")
         #redirect to the product page to see the inserted value
     return redirect(url_for('products'))
+
+
+@app.route('/update_products', methods=['GET', 'POST'])
+def update_products():
+
+    if request.method=='POST':
+        #get the inputs from the form using name attributes and store them in a variable
+        id=request.form['id']
+        pname=request.form['name']
+        bp=request.form['buyingprice']
+        sp=request.form['sellingprice']
+
+
+        update_product(pname,bp,sp,id)
+
+        flash('product update successfully','success')
+
+        return redirect(url_for('products'))
+    return redirect(url_for('products'))
+
+
+
+
 
 
          
@@ -40,13 +77,17 @@ def add_products():
 
 @app.route('/sales')
 def sales():
-    sale=fetch_data('sales')
-    #1.fetch products on the sales route for select element(part1.select element)
-    product=fetch_data('products')
-    #print (sale)
-    #2.pass the variable in reneder_template function(part 2.select element)
-    return render_template ('sales.html',sale=sale, products=product)
+    if 'email' in session:
 
+        sale=fetch_data('sales')
+        #1.fetch products on the sales route for select element(part1.select element)
+        product=fetch_data('products')
+        #print (sale)
+        #2.pass the variable in reneder_template function(part 2.select element)
+        return render_template ('sales.html',sale=sale, products=product)
+    else:
+        flash('log in to access this page',"warning")
+        return redirect(url_for('log_in'))
 #create a route that receives data from the form using a method
 @app.route('/add_sales',methods=['GET','POST'])
 #create the function to receive data from the modal on the form
@@ -63,7 +104,20 @@ def add_sales():
         # import from database.py &call the insert function to insert into the database
         insert_sales(new_sales)
         #redirect to the product page to see the inserted value
-    return redirect(url_for('sales'))    
+    return redirect(url_for('sales')) 
+
+@app.route('/update_sales',methods=['GET','POST'])
+def update_sale():
+    if request.method=='POST':
+        id=request.form['id']
+        quant=request.form['quantity']
+
+        update_sales(quant,id)
+
+        flash('sales quantity updated successfully')
+
+        return redirect(url_for('sales'))
+    return redirect(url_for('sales'))      
 
 
 
@@ -72,13 +126,17 @@ def add_sales():
 
 @app.route('/stocks')
 def stock():
-    stocks=fetch_data('stock')
-    #1.on stock route fetch products
-    products=fetch_data('products')
-    #print(stocks)
-    #2.pass the pvariable to render template
-    return render_template ('stock.html',stocks=stocks,product=products)
+    if 'email' in session:
 
+        stocks=fetch_data('stock')
+        #1.on stock route fetch products
+        products=fetch_data('products')
+        #print(stocks)
+        #2.pass the pvariable to render template
+        return render_template ('stock.html',stocks=stocks,product=products)
+    else:
+        flash('log in to access this page','warning')
+        return redirect(url_for('log_in'))
 #create a route that will receive data from the form to serverside with method
 @app.route('/add_stocks',methods=['GET','POST'])
 
@@ -96,8 +154,143 @@ def add_stock():
         #import the insert stock  function from database.py and call the function
         insert_stock(new_stock)
       #return/redirect the page to stock page by passing the function to fetch data for stock
-    return redirect(url_for('stock'))    
+    return redirect(url_for('stock'))   
+
+@app.route('/edit_stock', methods=['GET', 'POST'])
+def update_stocks():
+    if request.method=='POST':
+        id=request.form['id']
+        quant=request.form['s_quantity']
+        print(quant)
+
+        update_stock(quant,id)
+
+        flash('stock quantity updated successfully')
+
+        return redirect(url_for('stock'))
+    return redirect(url_for('stock'))
+
+    #create a route for dashboard
+     
+
+@app.route('/dashboard')
+def dashboard():
+    if 'email' in session:
+        rsales=recent_sales()
+        tpp=top_selling()
+        
+
+        mytotal=total_sales()
+        mytotal=float(mytotal)
+        myprofit=total_profit()
+        #profit per product
+        profit=get_profit()
+        #empty lists to store product name and profits
+        product_names=[]
+        product_profits=[]
+        
+        
+        for i in profit:
+            product_names.append(i[0])
+            product_profits.append(float(i[2]))
+    #sales per product
+        sale=get_sale()
+        print(sale)
+        pname=[]
+        sales=[]
+        
+        for i in sale:
+            pname.append(i[0])
+            sales.append(float(i[1]))
+
+
+        #sales per day
+        psale=sales_day()
+        dates=[]
+        dsale=[]
+        for i in psale:
+            dates.append(str(i[0]))
+            dsale.append(float(i[1]))
+
+        #profit per day
+        pro_day=profit_day()
+        date=[]    
+        dprofit=[]
+
+        for i in pro_day:
+            date.append(str(i[0]))
+            dprofit.append(float(i[1]))
+
+        #total profit per day for the card
+        tp_day=tprofit_day()    
+
+        #total sale per day for the card
+        ts_day=tsale_day()
+
+        #month extract
+        mo=month()
+    else:
+        flash('log in to access this page',"warning")    
+        return redirect(url_for('log_in'))
+        
+
+    return render_template('dashboard.html',mytotal_sale=mytotal,myprofits=myprofit,profit=profit,pnames=product_names, pprofit=product_profits, dates=dates,dsale=dsale,date=date,dprofit=dprofit,pname=pname,sales=sales,tp_day=tp_day,ts_day=ts_day,mo=mo,rsales=rsales,tpp=tpp)
+
+#log in route
+@app.route('/login',methods=['GET','POST'])
+def log_in():
+    if request.method=='POST':
+        mail=request.form['email']
+        password=request.form['password']
+
+        check=check_email(mail)
+        if check==None:
+            flash('Account not found.Register ',"danger")
+            return redirect(url_for('register'))
+        else:
+            if bycrypt.check_password_hash(check[3],password):
+              #adding email to session  
+                session['email']=mail
+                flash('log in successful', "success")
+                return redirect(url_for('dashboard')) 
+            else:
+                flash('wrong email or password', "danger")
+                return redirect(url_for('log_in'))    
+
+    return render_template('login.html')
+
+
+#register route
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        name=request.form['f_name'] 
+        mail=request.form['email']
+        password=request.form['password']
+
+        hashed_password=bycrypt.generate_password_hash(password).decode('utf-8')
+        new_user=(name,mail,hashed_password)
+        check=check_email(mail)
+        if check==None:
+            insert_users(new_user)
+            flash('user registered successfully',"success")
+
+            return redirect(url_for('log_in'))
+
+        else:
+            flash('user exists,use a different email or log in',"danger")    
+            return redirect(url_for('register'))
 
 
 
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    flash('you have been logged out',"info")
+
+    return redirect(url_for("log_in"))
 app.run(debug=True)
+
